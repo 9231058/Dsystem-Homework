dfc67c2d32b2f7a4d72549e8d2def1eeb689036f
Parham Alvani <parham.alvani@gmail.com>
2017-10-12 09:28:16 +0330

    Tests iter method.

--- project-1/src/example.com/clist/clist_test.go
+++ project-1/src/example.com/clist/clist_test.go
@@ -25,3 +25,38 @@ func TestAppend(t *testing.T) {
 
 	wg.Wait()
 }
+
+func TestIter(t *testing.T) {
+	var wg sync.WaitGroup
+
+	cl := New()
+
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		cl.PushBack(1)
+	}(&wg)
+	wg.Add(1)
+
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		cl.PushBack("Hi")
+	}(&wg)
+	wg.Add(1)
+
+	cl.PushBack(2)
+
+	cl.PushBack("Hello")
+
+	c := cl.Iter()
+	for e := range c {
+		t.Logf("%v\n", e.Value)
+	}
+
+	t.Logf("Waiting for all goroutines\n")
+	wg.Wait()
+
+	c = cl.Iter()
+	for e := range c {
+		t.Logf("%v\n", e.Value)
+	}
+}
