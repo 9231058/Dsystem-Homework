37abca4fc2bc954373494e5ee449fe15f570191c
Parham Alvani <parham.alvani@gmail.com>
2017-12-06 17:22:49 +0330

    Safe status with :lock:.

--- project-2/src/example.com/lsp/status.go
+++ project-2/src/example.com/lsp/status.go
@@ -1,12 +1,35 @@
 package lsp
 
-// Status represents server and connection state
-type Status int
+import "sync"
+
+type status struct {
+	s    int
+	lock *sync.RWMutex
+}
 
 const (
-	NotClosing Status = iota
-	StartClosing
-	HandlerClosed
-	ConnectionClosed
-	Closed
+	notClosing int = iota
+	startClosing
+	handlerClosed
+	connectionClosed
+	closed
 )
+
+func newStatus() *status {
+	return &status{
+		s:    notClosing,
+		lock: new(sync.RWLock),
+	}
+}
+
+func (s *status) set(s int) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.s = s
+}
+
+func (s *status) get() int {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	return s.s
+}
